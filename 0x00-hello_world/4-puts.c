1bf54694f745261ff04b1af77c8686a6113fcc4a
Your Name <mohammedrzq53@gmail.com>
2023-09-07 07:32:04 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
- * main - Entry point 
- * Description:  A C  program that print a line with puts function 
+ * main - Entry point
+ *
+ * Description:  A C  program that print a line with puts function
  *
  * Return: Always 0 (Success)
  */
