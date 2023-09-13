d459e8ce57a45c3652c84d101d7ef6c1403decfa
Your Name <mohammedrzq53@gmail.com>
2023-09-13 10:09:23 -0700

    C-function_nested_loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * _islower - Test wheter c is lowercase
+ * @c: Caracter to be checked 
+ *
+ * Return: 1 if c is lowercase, 0 otherwise.
+ */
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
