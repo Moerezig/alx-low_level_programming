d10e07db1f4e894c9ac568cd665223844314be78
Your Name <mohammedrzq53@gmail.com>
2023-09-19 23:47:06 -0700

    rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - Display a string s in reverse sense.
+ * @s: string to reverse
+ *
+ * Return: Always 0.
+ */
+
+void print_rev(char *s)
+{
+	int i, count = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		count++;
+	}
+	while (i--)
+	{
+		_putchar(*(--s));
+	}
+	_putchar('\n');
+}
