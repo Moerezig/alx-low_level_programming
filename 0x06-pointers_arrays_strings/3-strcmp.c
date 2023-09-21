ed0a3ab82201ce719a60a391ca4ca78e6160bbe7
Your Name <mohammedrzq53@gmail.com>
2023-09-21 06:10:08 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *_strcmp - Compare two string
+ * @s1: First string
+ * @s2: Second String
+ *
+ * Return: Integer number.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	char *r = s1;
+	char *l = s2;
+	int equal = 0;
+
+	while (*r || *l)
+	{
+		res = *r - *l;
+		if (equal != 0)
+			return (res);
+		r++;
+		l++;
+	}
+	return (equal);
+}
