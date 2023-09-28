24d86d444f7b8dfe46a62d9d0202bb4370f52ac1
Your Name <mohammedrzq53@gmail.com>
2023-09-28 10:44:05 -0700

    rec

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * aux_prime - prime numer
+ * @n:integer.
+ * @temp: integer.
+ * Return: values or -1.
+ */
+
+int aux_prime(int n, int temp)
+{
+	if (temp == 1)
+		return (1);
+	if (n % temp == 0)
+		return (0);
+	return (aux_prime(n, temp - 1));
+}
+
+/**
+ * is_prime_number - returns the natural square root of a number.
+ * @n: integer.
+ *
+ * Return: 1 if prime, 0 otherwise.
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (aux_prime(n, n - 1));
+}
