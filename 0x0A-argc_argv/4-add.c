3f3a0084f74e4e40b016c9dd900285e497dfe4cb
Your Name <mohammedrzq53@gmail.com>
2023-10-02 14:51:26 -0700

    lkjh

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,39 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-
 /**
- * main - entry point
- * @argc: number of arguments for the function
- * @argv: arguments for the function
- *
- * Return: always 0.
+ * isInteger - checks if s is an integer
+ * @s: string to check
+ * Return: 0 or 1
  */
 
-int main(int argc, char *argv[])
+int isInteger(const char *s)
 {
-	int i, j, sum = 0;
+int i = 0;
+while (s[i] != '\0')
+{
+	if (s[i] < '0' || s[i] > '9')
+		return (1);
+	i++;
+}
+return (0);
+}
 
-	if (argc == 1)
+/**
+ * main - adds positive numbers
+ * @argc: int
+ * @argv: list
+ * Return: 0
+ */
+
+int main(int argc, char const *argv[])
+{
+int sum = 0;
+while (--argc)
+{
+	if (isInteger(argv[argc]))
 	{
-		printf("0\n");
-		return (0);
+		printf("Error\n");
+		return (1);
 	}
-	else
-	{
-		for (i = 1; i < argc; i++)
-		{
-			for (j = 0; argv[i][j]; j++)
-			{
-				if (!isdigit(argv[i][j]))
-				{
-					printf("Error\n");
-					return (1);
-				}
-			}
-			sum += atoi(argv[i]);
-		}
-	}
-	printf("%d\n", sum);
-	return (0);
+	sum += atoi(argv[argc]);
+}
+
+printf("%i\n", sum);
+
+return (0);
 }
