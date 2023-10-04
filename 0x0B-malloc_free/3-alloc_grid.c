15c5a838137d697addde3383120d04afbda49e06
Your Name <mohammedrzq53@gmail.com>
2023-10-04 00:03:49 -0700

    ls

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -27,15 +27,16 @@ return (NULL);
 for (i = 0; i < height; i++)
 {
 grid[i] = malloc(sizeof(int) * width);
+}
 if (grid[i] == NULL)
-{
+
 for (i = i - 1; i >= 0; i--)
 {
 free(grid[i]);
 }
 free(grid);
 return (NULL);
-}
+{
 }
 for (i = 0; j < width; j++)
 grid[i][j] = 0;
