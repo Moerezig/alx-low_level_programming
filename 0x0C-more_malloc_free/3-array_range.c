c6a02d318c4d8448d9ce27ca65dc2fc84f2ad782
Your Name <mohammedrzq53@gmail.com>
2023-10-07 07:45:11 -0700

    looop

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+
+/**
+ * array_range - pointer.
+ * @min: minimun value
+ * @max: max value
+ *
+ * Return: pointer to an array contain integer number.
+ */
+
+int *array_range(int min, int max)
+{
+	int *array;
+	int L = 0, i;
+
+	if (min > max)
+		return (NULL);
+	array = malloc(sizeof(int) * (max - min + 1));
+	if (array == NULL)
+		return (NULL);
+	for (i = min; i <= max; i++)
+	{
+		array[L] = i;
+		L++;
+	}
+	return (array);
+}
