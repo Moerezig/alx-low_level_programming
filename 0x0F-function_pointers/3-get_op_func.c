8a582a4f9f1e2a3639eb4ee33f65aba352a06873
Your Name <mohammedrzq53@gmail.com>
2023-10-11 16:47:57 -0700

    poooooooooooooooooooooooooooooooooooooooooooooooointers fun

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * op_add - adds 5 functions
+ * @a: int a
+ * @b: int b
+ * Return: sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtracts
+ * @a: int a
+ * @b: int b
+ * Return: difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplies
+ * @a: int a
+ * @b: int b
+ * Return: multipliy
+ */
+int op_mul(int a, int b)
+
