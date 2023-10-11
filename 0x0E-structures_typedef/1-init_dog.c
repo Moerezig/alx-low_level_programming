9b42d8dca5206b376c948d292e3888ec8d01394a
Your Name <mohammedrzq53@gmail.com>
2023-10-10 23:50:23 -0700

    lkm

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+
+/**
+ * init_dog - init a dog struct type.
+ * @d: struct dog type
+ * @name: pet's name
+ * @age: pet's age
+ * @owner: owner's pet
+ *
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		(*d).name = name;
+		(*d).age = age;
+		(*d).owner = owner;
+	}
+}
