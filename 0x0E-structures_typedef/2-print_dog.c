bb93ac9fefc197ae7b23712c236b431932872f72
Your Name <mohammedrzq53@gmail.com>
2023-10-11 00:27:59 -0700

    re

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/*
+ * print_dog - print dog struct
+ *
+ * @d: dog struct 
+ *
+ * Return: nothing 
+ */
+void print_dog(struct dog *d)
+{
+
+	if ((*d) == '\0')
+		printf(nil);
+	if (name == '\0')
+		printf(name: (nil));
+	if if (d == '\0')
+                return;
+}
